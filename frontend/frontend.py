49a35c9b2edf37ade51c7ba66810b211ff770863
DrLuke <Lukas@Jackowski.de>
2018-01-27 18:49:38 +0100

    Frontend: Added OrdersList

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,12 +1,33 @@
+#!/bin/python3
+
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTableWidgetItem
+from PyQt5.QtCore import QTimer
 
-from frontend.frontendgui import Ui_MainWindow
+from frontendgui import Ui_MainWindow
 
+import math
 
 class ProgramState():
-    pass
+    class orderEntry:
+        def __init__(self):
+            self.name = "N/A"
+            self.price = 0.0
+            self.amount = 0
+
+    class accountsEntry:
+        def __init__(self):
+            self.name = "Max Mustermann"
+            self.balance = 0.0
+
+    def __init__(self):
+        self.orderList = []
+        self.accountList = []
+        self.buttonList = []
+
+    def pollState(self):
+        print("'polling'")
 
 class i6MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -15,26 +36,84 @@ class i6MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.state = ProgramState()
+        self.state.pollState()
+        self.state.buttonList = ["goo", "bar", "Joe", "Luke", "Christian", "Flurgeist", "Dreckige Toilette"]    # TEMP DEV LINE TODO: REMOVE
+        s1 = ProgramState.orderEntry()
+        s1.name = "HoLi"
+        s1.amount = 10
+        s1.price = 0.85
+        self.state.orderList = [
+            s1,
+            s1,
+            s1
+        ]
+
         self.buttons = []
+        self.lastButtonList = []    # Stores the last list of button names to be updated
+
+        self.updateButtons()
+        self.updateOrdersList()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timerCB)
+        self.timer.start(1000)
+
+    def timerCB(self, *args):
+        self.state.pollState()
+        self.updateButtons()
+        #self.updateOrdersList
+        #self.updateAccountsList
 
     def updateButtons(self):
-        clickableNames = ["goo", "bar", "Joe", "Luke", "Christian", "Flurgeist", "Dreckige Toilette"]
+        if self.state.buttonList == self.lastButtonList:    # Current button state is equal to previous, no need to update
+            return
 
-        for i in range(len(clickableNames)):
-            name = clickableNames[i]
+        for button in self.buttons:     # Delete old buttons
+            self.ui.buttonContainer.widget().layout().removeWidget(button)
+            button.deleteLater()
+
+        self.buttons = []
+
+        for i in range(len(self.state.buttonList)):
+            name = self.state.buttonList[i]
             newButton = QPushButton(name)
-            self.ui.buttonContainer.layout().addWidget(newButton, ())
-            newButton.clicked.connect(lambda x: self.NameButtonPressed)
+            self.ui.buttonContainer.widget().layout().addWidget(newButton, math.floor(i/2), i % 2)     # Add buttons in zig-zag pattern
 
-    def NameButtonPressed(self, index):
-        pass
+            self.buttons.append(newButton)
+
+            newButton.clicked.connect(self.NameButtonPressed)
+
+    def updateOrdersList(self):
+        self.ui.currentOrderList.clear()
+
+        self.ui.currentOrderList.setColumnCount(4)
+        self.ui.currentOrderList.setRowCount(len(self.state.orderList))
+
+        for i in range(len(self.state.orderList)):
+            order = self.state.orderList[i]
+            nameWidget = QTableWidgetItem(order.name)
+            self.ui.currentOrderList.setItem(i, 0, nameWidget)
+            amountWidget = QTableWidgetItem("%.2f" % order.amount)
+            self.ui.currentOrderList.setItem(i, 1, amountWidget)
+            priceWdiget = QTableWidgetItem("%.2f" % order.price)
+            self.ui.currentOrderList.setItem(i, 2, priceWdiget)
+            totalWidget = QTableWidgetItem("%.2f" % (order.price * order.amount))
+            self.ui.currentOrderList.setItem(i, 3, totalWidget)
+
+    def NameButtonPressed(self, *args):
+        clickedButton = self.sender()    # WTF why isn't this passed as argument?
+        clickedIndex = self.buttons.index(clickedButton)
+        print("Clicked: %s %s" % (clickedIndex, clickedButton.text()))
+
+        # TODO: RPC call to backend to let it know that button i was clicked
 
 
 
 
 
 app = QApplication(sys.argv)
-window = QMainWindow()
+window = i6MainWindow()
 
 window.show()
 sys.exit(app.exec_())
