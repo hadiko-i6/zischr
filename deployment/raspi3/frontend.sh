358f5317b12a2dd95e9a56a17881d123cda2ce36
Christian Schwarz <me@cschwarz.com>
2018-01-30 10:29:58 +0100

    deploy/raspi3: disable DPMS

--- deployment/raspi3/frontend.sh
+++ deployment/raspi3/frontend.sh
@@ -12,5 +12,7 @@ CURSOR=`xinput list | grep 3M | awk -F "id=" '{ print $2 }' | awk '{ print $1 }'
 xinput set-prop ${CURSOR} "libinput Calibration Matrix" -1.4143646408839778, 0.0, 1.2244475138121547, 0.0, 1.5525606469002695, -0.27324797843665766, 0.0, 0.0, 1.0
 #xinput set-prop ${CURSOR} "Evdev Axis Inversion" 0, 1
 
+xset s off -dpms
+
 exec python3 ../../frontend/frontend.py $@
 
